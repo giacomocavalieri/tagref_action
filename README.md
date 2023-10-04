b3d2a90f97d6f10b09897f4c86795435a59b65c4
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2023-10-04 10:26:52 +0200

    add `unused-tags` input

--- README.md
+++ README.md
@@ -10,6 +10,12 @@ It is the same as running:
 tagref check && tagref list-unused --fail-if-any
 ```
 
+## Inputs
+
+- `unused_tags`: can be used to change the action's behaviour with regard to unused tags
+  - `"forbid"` (the default): will make the CI fail if there's any note that's not referenced anywhere
+  - `"allow"`: will allow tags that have no references
+
 ## Example usage
 
 You can use this action as one of your CI steps like this:
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,35 @@
 #!/bin/sh -l
 
-# Install `tagref`
-curl https://raw.githubusercontent.com/stepchowfun/tagref/main/install.sh -LSfs | sh
+# SETUP ------------------------------------------------------------------------
 
-# Move to the repository's directory
+# This makes sure that if any of the checks fails the whole script will fail
+set -e
+
+# Move to the repository's directory to check its code
 cd /github/workspace
 
-# Check if all references are valid
-# and that all tags are actually used
-tagref check && tagref list-unused --fail-if-any
+
+# VALIDATE SCRIPT INPUTS -------------------------------------------------------
+
+if [[ $1 == "allow" ]]; then unused_tags="$1"
+elif [[ $1 == "forbid" ]]; then unused_tags="$1"
+else
+    echo "The \"unused_tags\" argument must be either \"allow\" or \"forbid\"" >&2
+    echo " - \"allow\" will allow notes that are not referenced" >&2
+    echo " - \"forbid\" will make the CI fail if there's any note that is not referenced" >&2
+    exit 1
+fi
+
+
+# TAGREF CHECKS ----------------------------------------------------------------
+
+# Install tagref
+curl https://raw.githubusercontent.com/stepchowfun/tagref/main/install.sh -LSfs | sh
+
+# Check all references are valid, this step can never be skipped 
+tagref check
+
+# Check all tags are actually referenced if the unused tags are not allowed
+if [[ $unused_tags == "forbid" ]]; then
+    tagref list-unused --fail-if-any
+fi
